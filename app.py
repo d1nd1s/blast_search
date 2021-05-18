065f21accbb0fd6f74f02afcb8a116032549ed84
Anthony Teslyuk <teslyuk@bio2.grid.kiae.ru>
2021-05-18 18:35:54 +0000

    Minimized code. Start operational

--- app.py
+++ app.py
@@ -5,93 +5,71 @@ from flask import Flask, render_template, redirect, session, url_for, flash, req
 from flask_bootstrap import Bootstrap
 from werkzeug.utils import secure_filename
 
+import subprocess
 
-from forms import MyForm
+from forms import SearchForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(32)
-
-UPLOAD_FOLDER = '/path/to/the/uploads'
-ALLOWED_EXTENSIONS = {'txt', 'fa', 'fasta'}
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 Bootstrap(app)
 
 
-@app.route('/')
-@app.route('/index')
+# UPLOAD_FOLDER = 'uploads'
+# ALLOWED_EXTENSIONS = {'txt', 'fa', 'fasta'}
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+  form = SearchForm()
+  if form.validate_on_submit():
+    cmd = ['blastn', '-db',  'db/'+form.data['search_db'], '-outfmt',  '0', '-max_target_seqs', str(form.data['max_target_sequences'])]
+    result = subprocess.run(cmd, input=form.data['sequence'].encode('utf8'), capture_output=True)
+    return render_template('result.html', data={'query': form.data['sequence'], 'result': result.stdout.decode('utf8'), 'db': form.data['search_db']})
+  return render_template('index.html', form=form)
+
+# def allowed_file(filename):
+#     return '.' in filename and \
+#            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# @app.route('/login', methods=["GET", "POST"])
+# def login():
+#     form = MyForm()
+#     if form.validate_on_submit():
+#       pass
+#     return return
+#     # def upload_file():
+#     #     if request.method == 'POST':
+#     #         # check if the post request has the file part
+#     #         if 'file' not in request.files:
+#     #             flash('No file part')
+#     #             return redirect(request.url)
+#     #         file = request.files['file']
+#     #         # if user does not select file, browser also submit an empty part without filename
+#     #         if file.filename == '':
+#     #             flash('No selected file')
+#     #             return redirect(request.url)
+#     #         if file and allowed_file(file.filename):
+#     #             filename = secure_filename(file.filename)
+#     #             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#     #             return redirect(url_for('uploaded_file',
+#     #                                     filename=filename))
+#     #     return
 
-
-@app.route('/login', methods=["GET", "POST"])
-def login():
-    form = MyForm()
-
-    def upload_file():
-        if request.method == 'POST':
-            # check if the post request has the file part
-            if 'file' not in request.files:
-                flash('No file part')
-                return redirect(request.url)
-            file = request.files['file']
-            # if user does not select file, browser also submit an empty part without filename
-            if file.filename == '':
-                flash('No selected file')
-                return redirect(request.url)
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                return redirect(url_for('uploaded_file',
-                                        filename=filename))
-        return
-
-    if form.validate_on_submit():
-        session['sequence'] = form.sequence.data
-        if not session['sequence']:
-            session['sequence'] = form.file.data
-        session['job_title'] = form.job_title.data
-        session['query_from'] = form.query_from.data
-        session['query_to'] = form.query_to.data
-        session['search_db'] = form.search_db.data
-        session['max_target_sequences'] = form.max_target_sequences.data
-        session['short_query'] = form.short_query.data
-        return redirect(url_for('success'))
-    return render_template('login.html', form=form)
-
-
-@app.route('/success')
-def success():
-    def subprocess_cmd(command):
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-        proc_stdout = process.communicate()[0].strip()
-        print(proc_stdout)
-
-
-    return render_template('success.html',
-                           # name=session['name'],
-                           # email=session['email'],
-                           # special=session['are_u_special']
-                           #
-                           search_db=session['search_db'],
-                           max_target_sequences=session['max_target_sequences'],
-                           short_query=session['short_query'],
-                           # result=session['sequence'],
-                           # result=subprocess.Popen(["blastn -version"], shell=True, stdout=subprocess.PIPE).stdout.read(),
-                           # result2=subprocess.Popen(["cd /home/dina"], shell=True, stdout=subprocess.PIPE).stdout.read(),
-                           # result3=subprocess.Popen(["ls"], shell=True, stdout=subprocess.PIPE).stdout.read(),
-                           result2=subprocess.Popen(["blastn -query /home/dina/blastdb/a.fa -db /home/dina/blastdb"], shell=True, stdout=subprocess.PIPE).stdout.read())
-
-# subprocess.run(["ls", "-l"])  # doesn't capture output
-# subprocess.CompletedProcess(args=['ls', '-l'], returncode=0)
-
-
+#     if form.validate_on_submit():
+#         session['sequence'] = form.sequence.data
+#         if not session['sequence']:
+#             session['sequence'] = form.file.data
+#         session['job_title'] = form.job_title.data
+#         session['query_from'] = form.query_from.data
+#         session['query_to'] = form.query_to.data
+#         session['search_db'] = form.search_db.data
+#         session['max_target_sequences'] = form.max_target_sequences.data
+#         session['short_query'] = form.short_query.data
+#         return redirect(url_for('success'))
+#     return render_template('login.html', form=form)
 
 if __name__ == '__main__':
     app.run()
--- forms.py
+++ forms.py
@@ -1,22 +1,25 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileRequired
-from wtforms import StringField, BooleanField, SubmitField, TextField, FileField, validators, TextAreaField, SelectField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, BooleanField, IntegerField, SubmitField, TextField, FileField, validators, TextAreaField, SelectField
+from wtforms.validators import DataRequired, Email, NumberRange
 from wtforms.fields.html5 import EmailField
 from wtforms.widgets import TextArea
 
-DB_CHOICES = ["Не выбрано", "Nucleotide", "WGS", "Reference Genome"]
+import os
+
+# DB_CHOICES = ["Не выбрано", "Nucleotide", "WGS", "Reference Genome"]
+DB_CHOICES = [l.name[:-4] for l in os.scandir('db') if l.name[-3:]=='nin']
 MAX_TAR_SEQ = ["Не выбрано", 10, 50, 100, 250, 500, 1000, 5000]
 
 
-class MyForm(FlaskForm):
-    sequence = TextAreaField('Введите последовательность', render_kw={"rows": 7, "cols": 11})
-    file = FileField('Или выберите файл', [validators.optional()])
+class SearchForm(FlaskForm):
+    sequence = TextAreaField('Последовательность для поиска', [validators.length(min=10, message='Минимальная длина строки для поиска: 10 нуклеотидов')], render_kw={"rows": 7, "cols": 11})
+    # file = FileField('Или выберите файл', [validators.optional()])
     # query subrange (a, b)
-    query_from = StringField('От', [validators.length(max=20)])
-    query_to = StringField('Дo', [validators.optional(), validators.length(max=20)])
+    # query_from = StringField('От', [validators.length(max=20)])
+    # query_to = StringField('Дo', [validators.optional(), validators.length(max=20)])
     # job title
-    job_title = TextField('Название запроса')
+    # job_title = TextField('Название запроса')
     # choose database
     search_db = SelectField('База данных, в которой будет производиться поиск',
                             choices=DB_CHOICES, validators=[DataRequired()])
@@ -25,9 +28,10 @@ class MyForm(FlaskForm):
     # limit to
     # choose a blast algorithm
     # algorithm parameters (выпадающий список)
-    max_target_sequences = SelectField('Максимальное число выдаваемых последовательностей',
-                                       [validators.optional()], choices=MAX_TAR_SEQ)
+    # max_target_sequences = SelectField('Максимальное число выдаваемых последовательностей',
+                                       # [validators.optional()], choices=MAX_TAR_SEQ)
+    max_target_sequences = IntegerField('Максимальное число выдаваемых последовательностей', default=10, validators=[NumberRange(0, 100, message='Введите число в диапазоне от 0 до 100')])
     short_query = BooleanField('Короткий запрос (запрос длиной до 30)')
-    e_value = StringField('')
+    # e_value = StringField('')
     # special_check = BooleanField('special')
-    submit = SubmitField('Submit')
+    # submit = SubmitField('Submit')
